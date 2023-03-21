7a366c973141e8f451c95b497d1b4fb833a2768c
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2023-03-21 10:52:15 -0300

    controller MVC

--- AppListaCurso/app/src/main/java/devandroid/machado/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/machado/applistacurso/controller/PessoaController.java
@@ -1,4 +1,11 @@
 package devandroid.machado.applistacurso.controller;
 
+import android.util.Log;
+
+import devandroid.machado.applistacurso.model.Pessoa;
+
 public class PessoaController {
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller","Controller iniciada..."+pessoa.toString());
+    }
 }
