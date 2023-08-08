2b3c4882a2fdb141d8458fc61aea489b598a5553
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2023-08-08 10:48:49 -0300

    Refatorando Layout para LinearLayout

--- AppListaCurso/app/src/main/java/devandroid/machado/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/machado/applistacurso/view/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
 
         controller = new PessoaController(MainActivity.this);
